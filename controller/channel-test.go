6c1375a327185af5aa969932635359ff7f2c598e
CaIon <i@caion.me>
2026-07-27 15:06:34 +0800

    feat: add New API channel support
    (cherry picked from commit 398cdafecf29f5211edd93cbb0525152299a6893)

--- controller/channel-test.go
+++ controller/channel-test.go
@@ -308,11 +308,10 @@ func testChannelWithKeyIndex(ctx context.Context, channel *model.Channel, testUs
 
 	apiType, _ := common.ChannelType2APIType(channel.Type)
 	if info.RelayMode == relayconstant.RelayModeResponsesCompact &&
-		apiType != constant.APITypeOpenAI &&
-		apiType != constant.APITypeCodex {
+		!common.IsResponsesCompactAPIType(apiType) {
 		return testResult{
 			context:     c,
-			localErr:    fmt.Errorf("responses compaction test only supports openai/codex channels, got api type %d", apiType),
+			localErr:    fmt.Errorf("responses compaction test is not supported for api type %d", apiType),
 			newAPIError: types.NewError(fmt.Errorf("unsupported api type: %d", apiType), types.ErrorCodeInvalidApiType),
 		}
 	}

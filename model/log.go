15e02d3b12743cf580a81cb3780289f48177702d
shiki <lvqishisama@gmail.com>
2026-09-02 03:37:55 +0800

    Revert "fix(log): preserve quota in usage statistics (#7108)"
    This reverts commit 6f8270fedc1903fa3718a5421cee63779b4ff5bc.

--- model/log.go
+++ model/log.go
@@ -1503,19 +1503,13 @@ func SumUsedQuota(logType int, startTimestamp int64, endTimestamp int64, modelNa
 		common.SysError("failed to query log stat: " + err.Error())
 		return stat, errors.New("查询统计数据失败")
 	}
-	var rateStat struct {
-		Rpm int
-		Tpm int
-	}
-	if err := rpmTpmQuery.Scan(&rateStat).Error; err != nil {
+	if err := rpmTpmQuery.Scan(&stat).Error; err != nil {
 		common.SysError("failed to query rpm/tpm stat: " + err.Error())
 		return stat, errors.New("查询统计数据失败")
 	}
 	if stat.TotalCount > 0 {
 		stat.SuccessRate = float64(stat.SuccessCount) / float64(stat.TotalCount) * 100
 	}
-	stat.Rpm = rateStat.Rpm
-	stat.Tpm = rateStat.Tpm
 
 	return stat, nil
 }
